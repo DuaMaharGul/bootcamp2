62383e96d5852f32ccb7b7783ed50e756b4f7040
Dua MG <duamahargul@outlook.com>
2022-02-08 19:39:23 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br/>
-          Update by Dua MG
+
+          Create React App by Dua MG
         </p>
         <a
           className="App-link"
